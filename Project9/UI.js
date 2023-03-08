93b657c8afadb469b6589772ce0515072314b863
Kenneth Nielsen <31967446+emmyapi@users.noreply.github.com>
2023-03-08 09:44:25 +0100

    Add files via upload

---
+++ Project9/UI.js
@@ -0,0 +1,50 @@
+export class UI {
+    constructor(game){
+        this.game = game;
+        this.fontSize = 30;
+        this.fontFamily = 'Creepster';
+        this.livesImage = document.getElementById('lives');
+    }
+    draw(context){
+        context.save();
+        context.shadowOffsetX = 2;
+        context.shadowOffsetY = 2;
+        context.shadowColor = 'red';
+        context.shadowBlur = 2;
+        context.font = this.fontSize + 'px ' + this.fontFamily;
+        context.textAlign = 'left';
+        context.fillStyle = this.game.fontColor;
+
+        // score
+        context.fillText(`Score: ${this.game.score}`,  10, 33);
+
+        // timer
+        context.font = this.fontSize * 0.8 + 'px ' + this.fontFamily;
+        //context.fillText(`Time Survived: ${(this.game.time * 0.001).toFixed(1)}`,  10, 63);
+        context.fillText(`Time Left: ${((this.game.maxTime * 0.001) - (this.game.time * 0.001)).toFixed(1)}`,  10, 63);
+
+        // lives
+        for (let i = 0; i < this.game.lives; i++) {
+            context.drawImage(this.livesImage, (this.game.width-33) - (33*i), 10, 25, 25);
+        } 
+
+        // game over message
+        if (this.game.gameOver) {
+            context.textAlign = 'center';
+            context.font = this.fontSize * 2 + 'px ' + this.fontFamily;
+            // win
+            if (this.game.score > 99){
+                context.fillText(`Boo-yah!`, this.game.width * 0.5, this.game.height * 0.5 - 20);
+                context.font = this.fontSize * 0.7 + 'px ' + this.fontFamily;
+                context.fillText(`Good job!`, this.game.width * 0.5, this.game.height * 0.5 + 20);
+            } else {
+                context.font = this.fontSize * 1.2 + 'px ' + this.fontFamily;
+                context.fillText(`Maybe you're just not cut out for the super hero stuff!`, this.game.width * 0.5, this.game.height * 0.5 - 20);
+                context.font = this.fontSize * 0.7 + 'px ' + this.fontFamily;
+                context.fillText(`Hit 'Enter' to try again!`, this.game.width * 0.5, this.game.height * 0.5 + 20);
+            }
+        }
+        context.restore();
+        
+    }
+}
---
+++ Project9/enemies.js
@@ -0,0 +1,96 @@
+class Enemy {
+    constructor(){
+        this.frameX = 0;
+        this.frameY = 0;
+        this.fps = 24;
+        this.frameInterval = 1000/this.fps;
+        this.frameTimer = 0;
+        this.markedForDeletion = false;
+    }
+    update(deltaTime){
+        // movement
+        this.x -= this.speedX + this.game.speed;
+        this.y += this.speedY;
+
+        // sprite animation
+        if (this.frameTimer < this.frameInterval) this.frameTimer += deltaTime
+        else {
+            if (this.frameX < this.maxFrame) this.frameX++;
+            else this.frameX = 0;
+            this.frameTimer = 0;
+        }
+
+        // delete passed enemies
+        if (this.x + this.width < 0) this.markedForDeletion = true;
+    }
+    draw(context){
+        if (this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
+        context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height)
+    }
+}
+
+export class FlyingEnemy extends Enemy {
+    constructor(game){
+        super();
+        this.game = game;
+        this.width = 60;
+        this.height = 44;
+        this.x = this.game.width;
+        this.y = Math.random() * this.game.height * 0.5;
+        this.speedProfile = Math.random() * 2 + 1.5
+        this.speedX = this.speedProfile;
+        this.speedY = 0;
+        this.maxFrame = 5;
+        this.image = document.getElementById('enemy_fly');
+        this.angle = 0;
+        this.va = Math.random() * 0.1 + 0.1;
+    }
+    update(deltaTime){
+        super.update(deltaTime);
+        this.angle += this.va;
+        this.y += Math.sin(this.angle);
+        this.speedX = this.speedProfile + (Math.random() * 4 - 2);
+    }
+}
+
+export class GroundEnemy extends Enemy {
+    constructor(game){
+        super();
+        this.game = game;
+        this.width = 60;
+        this.height = 87;
+        this.x = this.game.width;
+        this.y = this.game.height - this.height - this.game.groundMargin;
+        this.image = document.getElementById('enemy_plant');
+        this.speedX = 0;
+        this.speedY = 0;
+        this.maxFrame = 1;
+    }
+}
+
+export class ClimbingEnemy extends Enemy {
+    constructor(game){
+        super();
+        this.game = game;
+        this.width = 120;
+        this.height = 144;
+        this.x = this.game.width;
+        this.y = Math.random() * this.game.height * 0.5;
+        this.image = document.getElementById('enemy_spider_big');
+        this.speedX = 0;
+        this.speedY = Math.random() > 0.5 ? 1 : -1;
+        this.maxFrame = 5;
+    }
+    update(deltaTime){
+        super.update(deltaTime);
+        if (this.y > this.game.height - this.height - this.game.groundMargin) this.speedY *= -1;
+        if (this.y < -this.height) this.markedForDeletion = true;
+    }
+    draw(context){
+        super.draw(context);
+        context.beginPath();
+        context.moveTo(this.x + this.width/2,0);
+        context.lineTo(this.x + this.width/2, this.y + 50);
+        context.stroke();
+    }
+}
